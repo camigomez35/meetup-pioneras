2fc66737dbe4e5d68ccbc56d9e2f3a76b3dc1257
camigomez35 <camigomez35@gmail.com>
2017-06-25 14:19:16 -0500

    Modificación app.component y app.module

---
+++ src/app/dogs/dogs.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Dog } from '../dog';
+import { DogService } from '../dog.service';
+
+@Component({
+  selector: 'app-dogs',
+  templateUrl: './dogs.component.html',
+  styleUrls: ['./dogs.component.css']
+})
+
+export class DogsComponent implements OnInit {
+  selectedDog: Dog; //esta variable tipo Dog almacenará el dog que el usuario seleccione
+  dogs: Dog[]; //esta variable tipo array Dog almacenará los datos resultantes de la promesa de servicio DogService
+
+  constructor(private dogService: DogService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getDogs();
+  }
+
+  getDogs(): void {
+    this.dogService.getDogs().then(dogs => this.dogs = dogs);
+  }
+
+  onSelect(dog: Dog): void {
+    this.selectedDog = dog;
+  }
+}
